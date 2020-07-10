3582ede9e66fd03567f662e89e835a09ceaa9ac8
Yu-Hsiang Huang <yu-hsiang.huang@dlinkcorp.com>
2020-07-10 15:03:53 +0800

    Updates

---
+++ precache-manifest.0828696bfb97ffae607f5e5b757b2de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf2b9c53b501b80ef015fcaffbd7ac6",
+    "url": "/pathology-viewer/index.html"
+  },
+  {
+    "revision": "dd60cde5a1e63e9d1d20",
+    "url": "/pathology-viewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18046d0964519b304229",
+    "url": "/pathology-viewer/static/js/2.7e920ebf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pathology-viewer/static/js/2.7e920ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd60cde5a1e63e9d1d20",
+    "url": "/pathology-viewer/static/js/main.dc6cb981.chunk.js"
+  },
+  {
+    "revision": "17aabc074d5ff8d70a53",
+    "url": "/pathology-viewer/static/js/runtime-main.9220f8c9.js"
+  }
+]);
